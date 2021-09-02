ac4067c308b77c4b2f09e298b6fc2c78b4fa114f
prabid <prabid@umich.edu>
2021-09-01 22:20:33 -0400

    Return all players to home screen after end game

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ var io = socketIO(server);
 app.set("port", process.env.PORT || 5000);
 app.use("/static", express.static(__dirname + "/static"));
 // routing
-app.get("/", function(request, response) {
-  response.sendFile(path.join(__dirname, "index.html"));
+app.get("/", function (request, response) {
+    response.sendFile(path.join(__dirname, "index.html"));
 });
 // starts the server.
-server.listen(process.env.PORT || 5000, function() {
-  console.log("Starting server on port 5000");
+server.listen(process.env.PORT || 5000, function () {
+    console.log("Starting server on port 5000");
 });
 
 const NUM_ROUNDS = 4;
@@ -32,110 +32,113 @@ var value = 0;
 
 function set_lang(file) {
     // grab random lines from file and put in dictionary
-    return fs.readFileSync(path.resolve("./") + "/input/" + file, "utf-8").split("\n");
+    return fs
+        .readFileSync(path.resolve("./") + "/input/" + file, "utf-8")
+        .split("\n");
 }
 
 function get_lang() {
     if (game_type == "english") {
         return ENGLISH_LANG;
-    }
-    else if (game_type == "google") {
+    } else if (game_type == "google") {
         return GOOGLE_LANG;
     }
 }
 
-io.on("connection", function(socket) {
-  socket.on("new_player", function(new_player) {
-    let exists = false;
-    for (let key in players) {
-        if (players[key]["name"] === new_player) {
-            exists = true;
-        }
-    }
-    if (!in_game && !exists) {
-        num_players += 1;
-        players[socket.id] = {"score": 0, "name": new_player};
-        socket.emit("received_new_player", true);
-        player_names = [];
+io.on("connection", function (socket) {
+    socket.on("new_player", function (new_player) {
+        // Make sure two players don't use the same name
+        let exists = false;
         for (let key in players) {
-            player_names.push(players[key]["name"]);
+            if (players[key]["name"] === new_player) {
+                exists = true;
+            }
         }
-        io.sockets.emit("current_players", player_names);
-    }
-    else {
-        socket.emit("received_new_player", false);
-    }
-  });
-
-  socket.on("start_game", function(type) {
-    if (!validate_player(socket.id)) {
-        socket.emit("err", "not a current player in the game");
-        return;
-    }
-    in_game = true;
-    // send everyone the players
-    let scores = []
-    for (let key in players) {
-        scores.push([players[key]["name"], players[key]["score"]]);
-    }
-    for (let key in players) {
-        io.to(key).emit("results", -1, scores, [], undefined);
-    }
-    game_type = type;
-    begin_game();
-  });
-
-  socket.on("card_chosen", function(card_chosen) {
-    if (!validate_player(socket.id)) {
-        socket.emit("err", "not a current player in the game");
-        return;
-    }
-    // check to make sure they actually have that card, and that they haven"t already submitted a card
-    if (players[socket.id]["cards"].length != NUM_CARDS) {
-        return;
-    }
-
-    let valid = false;
-    let card_ind = -1;
-    for (let i = 0; i < players[socket.id]["cards"].length; i++) {
-        if (players[socket.id]["cards"][i][0] === card_chosen) {
-            card_ind = i;
-            valid = true;
+        if (in_game) {
+            socket.emit("received_new_player", false, false);
+        } else if (exists) {
+            socket.emit("received_new_player", false, true);
+        } else {
+            num_players += 1;
+            players[socket.id] = { score: 0, name: new_player };
+            socket.emit("received_new_player", true, false);
+            player_names = [];
+            for (let key in players) {
+                player_names.push(players[key]["name"]);
+            }
+            io.sockets.emit("current_players", player_names);
         }
-    }
-    if (!valid) {
-        return;
-    }
+    });
 
-    num_chosen += 1;
-    players[socket.id]["card_chosen"] = players[socket.id]["cards"][card_ind];
-    players[socket.id]["cards"].splice(card_ind, 1);
-    if (num_chosen == num_players) {
-        end_round();
-    }
-  });
+    socket.on("start_game", function (type) {
+        if (!validate_player(socket.id)) {
+            socket.emit("err", "not a current player in the game");
+            return;
+        }
+        in_game = true;
+        // send everyone the players
+        let scores = [];
+        for (let key in players) {
+            scores.push([players[key]["name"], players[key]["score"]]);
+        }
+        for (let key in players) {
+            io.to(key).emit("results", -1, scores, [], undefined);
+        }
+        game_type = type;
+        begin_game();
+    });
 
-  socket.on("continue", function() {
-    if (!validate_player(socket.id)) {
-        socket.emit("err", "not a current player in the game");
-        return;
-    }
+    socket.on("card_chosen", function (card_chosen) {
+        if (!validate_player(socket.id)) {
+            socket.emit("err", "not a current player in the game");
+            return;
+        }
+        // check to make sure they actually have that card, and that they haven't already submitted a card
+        if (players[socket.id]["cards"].length != NUM_CARDS) {
+            return;
+        }
+        let valid = false;
+        let card_ind = -1;
+        for (let i = 0; i < players[socket.id]["cards"].length; i++) {
+            if (players[socket.id]["cards"][i][0] === card_chosen) {
+                card_ind = i;
+                valid = true;
+            }
+        }
+        if (!valid) {
+            return;
+        }
 
-    if (round_num < NUM_ROUNDS) {
-        begin_round();
-    }
-    else {
-        end_game();
-    }
-  });
+        num_chosen += 1;
+        players[socket.id]["card_chosen"] =
+            players[socket.id]["cards"][card_ind];
+        players[socket.id]["cards"].splice(card_ind, 1);
+        if (num_chosen == num_players) {
+            end_round();
+        }
+    });
 
-  socket.on("terminate", function() {
-    in_game = false;
-    num_players = 0;
-    round_num = 0;
-    players = {};
-    num_chosen = 0;
-  });
+    socket.on("continue", function () {
+        if (!validate_player(socket.id)) {
+            socket.emit("err", "not a current player in the game");
+            return;
+        }
+
+        if (round_num < NUM_ROUNDS) {
+            begin_round();
+        } else {
+            end_game();
+        }
+    });
+
+    socket.on("terminate", function () {
+        in_game = false;
+        num_players = 0;
+        round_num = 0;
+        players = {};
+        num_chosen = 0;
+        io.sockets.emit("game_ended");
+    });
 });
 
 function validate_player(socket_id) {
@@ -152,19 +155,21 @@ function begin_game() {
         }
         players[key]["cards"] = cards;
 
-        io.to(key).emit("player_cards", players[key]["cards"].map(c => c[0]));
+        io.to(key).emit(
+            "player_cards",
+            players[key]["cards"].map((c) => c[0])
+        );
     }
     begin_round();
 }
 
 function begin_round() {
-    // "value" is random, or are parameters required?
     // deal out one card
     value = Math.ceil(Math.random() * 10);
     for (let key in players) {
         io.to(key).emit("new_val", round_num + 1, value);
     }
-};
+}
 
 function end_round() {
     let chosen = calculate_scores();
@@ -180,31 +185,32 @@ function end_round() {
     }
     round_num += 1;
     num_chosen = 0;
-};
+}
 
 // returns list of cards chosen
 function calculate_scores() {
     // check dictionary for who had the highest score
     // add appropriate value to top player
     let chosen = [];
-    let max_player = "";
-    let max_number = 1000000;
+    let min_player = "";
+    let min_number = Infinity;
     for (let key in players) {
         let card_chosen = players[key]["card_chosen"];
-        chosen.push([players[key]["name"], card_chosen[0], card_chosen[1]]);
-        if (card_chosen[1] < max_number) {
-            max_number = card_chosen[1];
-            max_player = key;
+        // A lower index is a more popular card, therefore a winner
+        if (card_chosen[1] < min_number) {
+            min_number = card_chosen[1];
+            min_player = key;
         }
+        chosen.push([players[key]["name"], card_chosen[0], card_chosen[1]]);
     }
-    players[max_player]["score"] += value;
-    chosen.sort(function(a,b) {
-        a = a[2];
-        b = b[2];
-        return a < b ? -1 : (a > b ? 1 : 0);
+    players[min_player]["score"] += value;
+    chosen.sort(function (a, b) {
+        a_val = a[2];
+        b_val = b[2];
+        return a_val < b_val ? -1 : a_val > b_val ? 1 : 0;
     });
     return chosen;
-};
+}
 
 function end_game() {
     // reset all values accordingly
--- static/game.js
+++ static/game.js
@@ -3,39 +3,122 @@ var socket = io();
 var socket_id = "";
 var player_name = "";
 
-window.onload = function() {
-    $("#name").focus();
-}
+window.onload = function () {
+    set_join_game_page();
+};
 
-function on_enter(e, func) { 
+function on_enter(e, func) {
+    const ENTER_KEY = 13;
     // look for window.event in case event isn't passed in
     e = e || window.event;
-    if (e.keyCode == 13) {
+    if (e.keyCode == ENTER_KEY) {
         func();
         return false;
     }
     return true;
 }
 
-socket.on("received_new_player", (accepted) => {
+function set_join_game_page() {
+    $(".nameForm").show();
+    $("#name").focus();
+}
+function set_undo_join_game_page() {
+    $(".nameForm").hide();
+}
+
+function set_waiting_room_accepted_page() {
+    $("#begin").show();
+    $(".startBtn").focus();
+}
+function set_waiting_room_name_already_taken_page() {
+    $("#nameAlreadyTaken").show();
+}
+function set_waiting_room_unaccepted_page() {
+    $("#alreadyInProgress").show();
+    $("#end").show();
+}
+function set_undo_waiting_room_page() {
+    $("#begin").hide();
+
+    $("#nameAlreadyTaken").hide();
+
+    $("#alreadyInProgress").hide();
+    $("#end").hide();
+}
+
+function set_scores_page() {
+    $("#end").show();
+    $(".scoreboardHeader").show();
+}
+function set_undo_scores_page() {
+    $("#end").hide();
+    $(".scoreboardHeader").hide();
+}
+
+function set_choosing_page() {
+    $("#info").show();
+
+    // Allows players to select a card using the left and right arrow keys
+    $("body").keydown(function (e) {
+        const LEFT_KEY = 37;
+        const RIGHT_KEY = 39;
+
+        if (e.keyCode !== LEFT_KEY && e.keyCode !== RIGHT_KEY) {
+            return;
+        }
+
+        // If no card is currenly selected, focus on the first one
+        if ($(":focus[id*='card-']").length === 0) {
+            $("#card-0").focus();
+            return;
+        }
+
+        let focus_num = Number($(":focus").attr("id").replace("card-", ""));
+        if (e.keyCode == LEFT_KEY) {
+            $(
+                "#card-" + ((focus_num + NUM_CARDS - 1) % NUM_CARDS).toString()
+            ).focus();
+        } else if (e.keyCode == RIGHT_KEY) {
+            $("#card-" + ((focus_num + 1) % NUM_CARDS).toString()).focus();
+        }
+    });
+}
+function set_undo_choosing_page() {
+    $("#info").hide();
+    $("body").off("keydown");
+}
+
+function set_results_page() {
+    $("#results").show();
+    $("#continue").focus();
+}
+function set_undo_results_page() {
+    $("#results").hide();
+    $(".chosen").empty();
+}
+
+socket.on("received_new_player", (accepted, already_taken) => {
     socket_id = socket.id;
-    if (!accepted) {
-        $("#noJoin").show();
-        $("#end").show();
-    }
-    else {
-        $("#begin").show();
-        $(".startBtn").focus();
+    if (accepted) {
+        set_waiting_room_accepted_page();
+    } else if (already_taken) {
+        set_waiting_room_name_already_taken_page();
+    } else {
+        set_waiting_room_unaccepted_page();
     }
 });
 
 socket.on("current_players", (player_names) => {
-    console.log(player_names);
     $("#waitingRoomPlayers").empty();
-    player_names.forEach(p => {
+
+    player_names.forEach((p) => {
         let name = document.createElement("div");
         name.classList.add("waitingListPlayer");
-        name.append(document.createElement("span").appendChild(document.createTextNode(p)));
+        name.append(
+            document
+                .createElement("span")
+                .appendChild(document.createTextNode(p))
+        );
 
         $("#waitingRoomPlayers").append(name);
     });
@@ -43,8 +126,10 @@ socket.on("current_players", (player_names) => {
 
 function submit_name() {
     player_name = $("#name").val();
-    socket.emit("new_player", player_name);
-    $(".nameForm").hide();
+    if (player_name != "") {
+        socket.emit("new_player", player_name);
+        set_undo_join_game_page();
+    }
 }
 
 function start_game() {
@@ -60,97 +145,97 @@ function card_chosen(event) {
 }
 
 // get value for round
-socket.on("new_val", function(round_num, value) {
-    $("#round_num").text("Round " + round_num.toString());
-    $("#results").hide();
-    $("#info").show();
-    $("body").keydown(function(e) {
-        if (e.keyCode !== 37 && e.keyCode !== 39) {
-            return;
-        }
-        if ($(":focus[id*='card-']").length === 0) {
-            $("#card-0").focus();
-            return;
-        }
-        let focus_num = Number($(":focus").attr("id").replace('card-', ''));
-        if (e.keyCode == 37) { // left
-          $("#card-" + ((focus_num + NUM_CARDS - 1) % NUM_CARDS).toString()).focus();
-        }
-        else if (e.keyCode == 39) { // right
-          $("#card-" + ((focus_num + 1) % NUM_CARDS).toString()).focus();
-        }
-      });
+socket.on("new_val", function (round_num, value) {
+    $("#roundNum").text("Round " + round_num.toString());
+    set_undo_results_page();
+    set_choosing_page();
     $("#value").text(value.toString());
-})
+});
 
 // on "results", show current scores
-socket.on("results", function(round_num, results, chosen, new_card) {
+socket.on("results", function (round_num, results, chosen, new_card) {
     $("#scores > tbody").empty();
     if (round_num !== -1) {
-        $(".chosen").empty();
-        $("#end").show();
-        $("#results").show();
-        $("#info").hide();
-        $("body").off("keydown");
-        $("#continue").focus();
+        set_results_page();
+        set_undo_choosing_page();
 
         let button = $(".card[disabled]");
         button.empty();
         button.val(new_card);
         button.text(new_card);
         button.prop("disabled", false);
+        $("#roundNum").text("Round " + round_num.toString() + " Results");
     }
-
-    $("#round_num").text("Round " + round_num.toString() + " Results");
-    results.forEach(r => {
+    results.forEach((r) => {
         let score = document.createElement("tr");
-        let score_player = $(document.createElement("td")).text(r[0])
+        let score_player = $(document.createElement("td")).text(r[0]);
         let score_points = $(document.createElement("td")).text(r[1]);
         if (r[0] === player_name) {
-            $(score_player).css("font-weight","Bold");
+            $(score_player).css("font-weight", "Bold");
         }
         $(score).append(score_player);
         $(score).append(score_points);
         $("#scores > tbody").append(score);
     });
 
-    let winner = $(".winner");
+    let winner = $(".winnerName");
     winner.empty();
     if (chosen.length > 0) {
-        winner.append(document.createTextNode(chosen[0][0]))
+        winner.append(document.createTextNode(chosen[0][0]));
     }
 
-    chosen.forEach(c => {
+    chosen.forEach((c) => {
         let chose = document.createElement("div");
         chose.classList.add("chosenItem");
 
         let chose_name = document.createElement("div");
-        chose_name.append(document.createElement("p").appendChild(document.createTextNode(c[0])));
+        chose_name.append(
+            document
+                .createElement("p")
+                .appendChild(document.createTextNode(c[0]))
+        );
         chose_name.classList.add("chosenName");
 
         let chose_word = document.createElement("div");
-        chose_word.append(document.createElement("p").appendChild(document.createTextNode(c[1])));
+        chose_word.append(
+            document
+                .createElement("p")
+                .appendChild(document.createTextNode(c[1]))
+        );
         chose_word.classList.add("chosenWord");
 
-        let chose_pop = document.createElement("div");
-        chose_pop.append(document.createElement("p").appendChild(document.createTextNode(c[2])));
-        chose_pop.classList.add("chosenPop");
+        let chose_val = document.createElement("div");
+        chose_val.append(
+            document
+                .createElement("p")
+                .appendChild(document.createTextNode(c[2]))
+        );
+        chose_val.classList.add("chosenPop");
 
         if (c[0] === player_name) {
-            $(chose_name).css("font-weight","Bold");
+            $(chose_name).css("font-weight", "Bold");
         }
         $(chose).append(chose_name);
         $(chose).append(chose_word);
-        $(chose).append(chose_pop);
+        $(chose).append(chose_val);
 
         $(".chosen").append(chose);
     });
 
     // prevents lag on first round
     if (round_num !== -1) {
-        $(".header").show();
+        $(".scoreboardHeader").show();
     }
-})
+});
+
+socket.on("game_ended", () => {
+    set_undo_waiting_room_page();
+    set_undo_scores_page();
+    set_undo_results_page();
+    set_undo_choosing_page();
+
+    set_join_game_page();
+});
 
 function cont() {
     socket.emit("continue");
@@ -158,26 +243,16 @@ function cont() {
 
 function terminate() {
     socket.emit("terminate");
-    $(".header").hide();
-    $("#results").hide();
-    $(".nameForm").show();
-    $("#name").focus();
-    $("#end").hide();
-    $("#info").hide();
-    $("#noJoin").hide();
-
-    // remove event listener
-    $("body").off("keydown");
 }
 
 // finish
-socket.on("end_game", function(winner) {
-    $("#round_num").text(winner + " won the game!");
-    $("#results").hide();
-})
+socket.on("end_game", function (winner) {
+    $("#roundNum").text(winner + " won the game!");
+    set_undo_results_page();
+});
 
 // on receiving initial cards
-socket.on("player_cards", function(cards) {
+socket.on("player_cards", function (cards) {
     $("#cards").empty();
     for (let i = 0; i < NUM_CARDS; ++i) {
         let button = document.createElement("button");
@@ -185,13 +260,12 @@ socket.on("player_cards", function(cards) {
         button.classList.add("card");
         button.value = cards[i];
         button.appendChild(document.createTextNode(cards[i]));
-        button.onclick = function() {
+        button.onclick = function () {
             card_chosen(event);
-        }
+        };
         $("#cards").append(button);
     }
-    $("#begin").hide();
-    $(".chosen").empty();
-    $("#end").show();
-    $(".header").show();
+
+    set_undo_waiting_room_page();
+    set_scores_page();
 });
